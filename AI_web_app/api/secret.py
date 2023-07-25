cff4657d3c06b97d4a04359733437d9d71dc0e6b
Seongjin Lee <lsjg9909@naver.com>
2023-07-25 18:17:40 +0900

    Delete secret.py

--- AI_web_app/api/secret.py
+++
@@ -1,7 +0,0 @@
-### API SCERET
-### 이 파일에는 openai와 ncloud ocr api를 사용하기 위한 api key가 저장되어 있습니다.
-### Github 등 공개된 장소에 절대 업로드하지 마세요!
-### 현재 레포지토리에서는 .gitignore 파일로 인해 수동으로 추가하지 않는 한 커밋되지 않습니다.
- 
-OPENAI_API_KEY = "OPENAI_API_KEY"
-NCLOUD_OCR_API_KEY = "NCLOUD_OCR_API_KEY"
