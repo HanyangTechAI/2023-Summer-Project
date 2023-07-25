1ce68d21773e42a2eae59bf851d68574246ffbaf
GirinMan <lsjg9909@naver.com>
2023-07-25 17:53:39 +0900

    Add code skeleton

---
+++ AI_web_app/README.md
@@ -0,0 +1,14 @@
+# 2023 HAI 여름 WEB APP 개발 과제 데모 코드
+## 개요
+- AI 웹 데모 앱 개발을 연습할 수 있는 프로젝트 예시 코드입니다.
+- text2text, text2img, img2img, img2text 4가지 API가 준비되어 있습니다.
+
+## 앱 실행 방법
+1. `pip install -r requirements.txt` 명령을 실행하여 필요한 패키지를 설치합니다.
+2. 현재 레포지토리를 clone한 디렉토리에 들어갑니다.
+3. `streamlit run st_app.py` 명령을 실행하면 localhost:8501 에서 실행 중인 앱에 접속할 수 있습니다.
+
+## 스크린샷
+|text2text, text2img|img2img|img2text|
+| --- | --- | --- |
+| ![image](resources/demo_1.png) | ![!image](resources/demo_2.png) | ![!image](resources/demo_3.png) |
---
+++ AI_web_app/example_streamlit_app.py
@@ -0,0 +1,64 @@
+
+from api.example import ExampleAPI
+import streamlit as st
+from PIL import Image
+
+
+@st.cache_resource
+def get_api():
+    return ExampleAPI(font="resources/malgun.ttf")
+
+
+def main():
+    api = get_api()
+
+    st.title("2023 HAI 여름방학 Web APP 개발 과제")
+
+    # Text2Text API 활용
+    st.subheader("1. Text to Text API 활용")
+    st.markdown("- 텍스트를 입력받아 텍스트를 출력하는 API를 사용해 보겠습니다.\n- 텍스트를 입력하면 줄 바꿈과 **HAI is the best!!** 를 추가해줍니다.")
+
+    query = st.text_input('Input Text', "여기에 텍스트 입력", key="text2text")
+    response = api.query_text2text(query)
+
+    st.markdown("**API Output**")
+    st.code(f"{response}", language="csv")
+
+    # Text2Image API 활용
+    st.subheader("2. Text to Image API 활용")
+    st.markdown("- 텍스트를 입력받아 이미지를 출력하는 API를 사용해 보겠습니다.\n- 입력된 텍스트가 그려진 비트맵 이미지가 생성됩니다.")
+
+    query = st.text_input('Input Text', "Hello, World!", key="text2image")
+    response = api.query_text2image(query, size=(250, 150))
+
+    st.markdown("**API Output**")
+    st.image(response)
+
+    # Image2Image API 활용
+    st.subheader("3. Image to Image API 활용")
+    st.markdown("- 이미지를 입력받아 이미지를 출력하는 API를 사용해 보겠습니다.\n- 입력된 이미지를 그레이스케일로 바꾼 이미지가 생성됩니다.")
+
+    query = st.file_uploader('Input Image', key='image2image')
+    if query is not None:
+        st.image(query)
+        response = api.query_image2image(query)
+        st.markdown("**API Output**")
+        st.image(response)
+
+    # Image2Text API 활용
+    st.subheader("4. Image to Text API 활용")
+    st.markdown("- 이미지를 입력받아 텍스트를 출력하는 API를 사용해 보겠습니다.\n- 입력된 이미지의 해상도와 같은 주요 정보를 출력해줍니다.")
+    query = st.file_uploader('Input Image')
+    if query is not None:
+        st.image(query)
+        response = api.query_image2text(query, key='image2text')
+        st.markdown("**API Output**")
+        st.code(f"{response}", language="python")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
